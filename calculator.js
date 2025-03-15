0e17ae76648f19e5280e6eff1fe47ff71153737f
Shyaar Sati Fwatshak <shyaarsati@gmial.com>
2025-03-15 23:47:50 +0100

    buton/created

--- calculator.js
+++ calculator.js
@@ -1,7 +1,21 @@
-let keys = [7,8,9,4,5,6,1,2,3,"+","-","/","*","AC","+/-","%"];
-let 
+const nums = [
+    "AC","+/-","%","/",
+    7,8,9,"*",
+    4,5,6,"-",
+    1,2,3,"+",
+    "",".",0,"="
+];
+const rS = ["/","*","-","+","="]  
+const tS = ["AC","+/_","%"]  
 
-for(i =0; i<keys.length; i++){
+for(i =0; i<nums.length; i++){
+    let val = nums[i];
+    let numBtns = document.createElement('button')
+    numBtns.className = "bg-black p-6 rounded-full text-white w-[70px] h-[70px]"
+    numBtns.innerText = val
+    document.getElementById('keys').appendChild(numBtns)
 
 }
-f.innerText = keys
+
+
+
